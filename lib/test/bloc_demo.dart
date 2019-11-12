88ed64b2b8a438247e781c19c703e5333e6be56b
yangtianci <yangtianci1@daojia.com.cn>
2019-11-12 17:18:06 +0800

    inheritedWidget,streamController.sink\listen

--- lib/test/bloc_demo.dart
+++ lib/test/bloc_demo.dart
@@ -1,21 +1,87 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: 'bloc',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('bloc训练'),
+/**
+ * bloc = Business Logic Component  业务逻辑组件
+ */
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'bloc',
+      home: CounterProvider(
+        bloc: BlocCounter(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('bloc训练'),
+          ),
+          body: BlocDemo(),
+          floatingActionButton: CounterActionButton(),
+        ),
       ),
-      body: BlocDemo(),
     ),
-  ));
+  );
 }
 
-class BlocDemo extends StatelessWidget{
+class CounterProvider extends InheritedWidget {
+  //继承部件
+  final Widget child;
+  final BlocCounter bloc;
+
+  CounterProvider({this.child, this.bloc}) : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
 
   @override
-  Widget build(BuildContext context) {
+  bool updateShouldNotify(CounterProvider oldWidget) {
+    return true;
+  }
+}
 
+class BlocCounter {
+  final  _streamController  = StreamController<int>();
+  StreamSink<int> get counter => _streamController.sink;
+
+
+  BlocCounter(){
+    this._streamController.stream.listen(onData);
+  }
+  void onData(int data){
+    print('$data');
+  }
+
+  void log() {
+    print('Bloc');
+  }
+}
+
+class CounterActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    BlocCounter _bloc = CounterProvider.of(context).bloc;
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        _bloc.counter.add(1);
+        _bloc.log();
+      },
+    );
+  }
+}
+
+class BlocDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    BlocCounter _bloc = CounterProvider.of(context).bloc;
+    return Center(
+      child: ActionChip(
+        label: Text('0'),
+        onPressed: () {
+          _bloc.counter.add(1);
+          _bloc.log();
+        },
+      ),
+    );
   }
 }
